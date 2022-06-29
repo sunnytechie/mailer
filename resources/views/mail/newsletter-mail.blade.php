c79cc999a3d84597b6bc54c2e422fe9cb7881ee3
SunnyTechie <sunnyict001@gmail.com>
2022-06-29 17:01:10 +0100

    Newsleter change

--- resources/views/mail/newsletter-mail.blade.php
+++ resources/views/mail/newsletter-mail.blade.php
@@ -4,7 +4,6 @@
     }
 </style>
 @component('mail::message')
-<img style="margin-bottom: 15px" src="https://scontent.fabb1-2.fna.fbcdn.net/v/t39.30808-6/286179978_10219562617269899_979001202881814094_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeFH9qCarywE-Pua7ecBV5SAw-y-UwVrVPjD7L5TBWtU-AG5j7MkIHmUJw1LoyGBSJI&_nc_ohc=P7YMcKknvCoAX9fHVo2&_nc_zt=23&_nc_ht=scontent.fabb1-2.fna&oh=00_AT8zAgSG1JoIDCJaOiPbomavonoY8oLgfSqv0espO1re-Q&oe=62A3123E" alt="Loading Image">
 
 {!! $compose['message'] !!}
 
