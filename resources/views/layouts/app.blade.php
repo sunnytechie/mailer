a659d26d1bf6828b376536c0d1c7bf78616e1ff3
SunnyTechie <sunnyict001@gmail.com>
2022-06-29 16:41:23 +0100

    Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'SunnyTechie') }}</title>
 
             <!-- Favicon -->
-        <link href="{{ asset('assets/img/mailer_favicon-removebg-preview.png') }}" rel="icon">
+            <link rel="shortcut icon" href="breeze/assets/images/logo.png" />
 
         <!-- Icon Font Stylesheet -->
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
