158495d26de3e82d4b934eda7854c9682bc7e30f
SunnyTechie <sunnyict001@gmail.com>
2022-07-01 16:10:59 +0100

    reformed

--- resources/views/mail/newsletter-mail.blade.php
+++ resources/views/mail/newsletter-mail.blade.php
@@ -6,13 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <style>
-        /* Hide spacer on screen resolution of 640px */
         @media screen and (max-width: 640px) {
             .spacer {
                 display: none;
             }
         }
-
         .msg p {
             font-size: 16px;
             line-height: 24px;
@@ -20,37 +18,20 @@
     </style>
 </head>
 <body style="background: #E3E3E3">
-
     <div class="spacer" style="height: 20px"></div>
-    
     <div style="width: 640px; margin: 0 auto; background: #fff">
-       
         <div style="width: 100%; background: #ffffff; margin-bottom: 5px; text-align: center; padding: 15px; border-bottom: 0.1rem solid #aaa;">
             <img class="img-thumbnail" height="60px" width="60px" src="https://sfiloveinaction.org/wp-content/uploads/2022/07/cropped-SFI-Logo.png" alt="Loading">
         </div>
-
         <div class="msg" style="padding: 15px 18px; font-size: 14px;">
-    
             {!! $compose['message'] !!}
-            <p>We have entered the 2nd half of the year; the month of July; which is our month of FAITH. </p>
-            <p>Faith is an indispensable act required for the existence of every Christian which if void, separates one from God. It is a necessary ingredient for life. Dare to have and increase your faith this month. </p>
-            <p>The month of July is a special month in the ministry of SFI. This is because it's our founder and Mentor's birth month. And on the 1st of and 2nd of July, Sisters Fellowship International will be celebrating the birthday of an Icon, Rev Mrs Nches Iredu. There will be amazing activities such as; praise night, song ministrations, dance, drama and lots more.</p>
-            <p>And on the 6th of July Sister Fellowship International Hqtrs will also be celebrating the birthday of a Rare Gem. She is indeed a unique and wonderful mother, a spiritual leader who nurtures everyone to become a better version of themselves. </p>
-            <p>In Sister Fellowship International Young Daughters Forum Calendar, the month of July is the month for a book review. This is an opportunity where reads a particular book and also review the book. Different expositions and understanding are shared and in the end, more insight is gained and lives impacted. </p>
-            <p>This month, raise your faith for the miraculous. Stay tuned and connected to God. Don't miss out on anything, it's sure going to be a month filled with pleasant surprises for you.</p>
-            <p>cheers!!!</p>
-            
         </div>
-
-       
         <div style="background: #f8eaea; padding: 30px; text-align: center; border-bottom: 0.1rem solid #fff; border-top: 0.1rem solid #fff;">
             <div style="margiin-bottom: 5px; text-align: center">Get our God's heartbeat mobile app</div>
             <br>
             <a style="margin-right: 10px" href="https://apps.apple.com/gb/app/sfghb/id1564125722"><img style="border-radius: 0.2rem;" src="https://sfiloveinaction.org/wp-content/uploads/2022/06/Untitled-design-17.png" alt=""></a>
             <a href="https://play.google.com/store/apps/details?id=godsheartbeat.net"><img style="border-radius: 0.2rem" src="https://sfiloveinaction.org/wp-content/uploads/2022/06/Untitled-design-18.png" alt=""></a>
         </div>
-
-        
         <footer style="background: #f8eaea; padding: 20px 0;">
             <div style="padding: 5px 20px">
                 <a style="font-size: 12px; text-decoration: none; border-bottom: 0.1rem solid #ddd; color: #aaa;" href="#">Unsubscribe</a> | <a style="font-size: 12px; text-decoration: none; border-bottom: 0.2rem solid #ddd; color: #aaa;" href="https://www.sfiloveinaction.org/">Privacy Policy</a> <br>
@@ -64,6 +45,5 @@
         </footer>
     </div>
     <div class="spacer" style="height: 20px"></div>
-    
 </body>
 </html>
